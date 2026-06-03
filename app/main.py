29aeb1aea7aaa467c6cb771e3ceb13d24ef002ba
Török_Tamás <torokt97@gmail.com>
2026-06-03 09:56:16 +0200

    Add alerts endpoint

--- app/main.py
+++ app/main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+from app.parser import extract_ips
+from app.detector import detect_bruteforce
+
+app = FastAPI(
+    title="Cyber Log Analyzer"
+)
 
 @app.get("/")
 def root():
@@ -8,3 +13,13 @@ def root():
         "status": "running",
         "project": "Cyber Log Analyzer"
     }
+
+@app.get("/alerts")
+def alerts():
+
+    ips = extract_ips("logs/sample_auth.log")
+
+    return detect_bruteforce(
+        ips,
+        threshold=5
+    )
